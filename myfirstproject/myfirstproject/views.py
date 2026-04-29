d003950a26f287276432205ab43316f223224255
glufy100 <119037482+glufy100@users.noreply.github.com>
2026-04-29 10:50:29 +0200

    feuyzbfzh

--- myfirstproject/myfirstproject/views.py
+++ myfirstproject/myfirstproject/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from myfirstproject import myfirstproject
+
 
 def index(request):
     return render(request, 'myfirstproject.html')
+
+
+def page2(request):
+    """Renvoie la page statique myfirstproject2.html située dans le dossier de templates.
+
+    Le dossier des templates inclut déjà `BASE_DIR / 'myfirstproject'` dans `settings.TEMPLATES['DIRS']`,
+    donc le fichier `myfirstproject2.html` peut être rendu directement.
+    """
+    return render(request, 'myfirstproject2.html')
+
+
+def bonjour(request):
+    nom = request.GET["nom"]
+    return render(request, "myfirstproject/bonjour.html" , {"nom": nom})
