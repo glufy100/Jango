bfaa605a4db17dd6e5e96b72836d9852c7782882
glufy100 <119037482+glufy100@users.noreply.github.com>
2026-05-06 15:28:17 +0200

    fini

--- myfirstproject/myfirstapp/urls.py
+++ myfirstproject/myfirstapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
-    path('bonjour/', views.bonjour),
-    path('formulaire/', views.formulaire),
+    path('index/', views.index, name='index'),
+    path('bonjour/', views.bonjour, name='bonjour'),
+    path('formulaire/', views.formulaire, name='formulaire'),
 ]
--- myfirstproject/myfirstapp/views.py
+++ myfirstproject/myfirstapp/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'myfirstapp/index.html')
+
+def formulaire(request):
+    """Afficher le formulaire simple (méthode GET dans le template actuel)."""
+    return render(request, 'myfirstapp/formulaire.html')
+
+
+def bonjour(request):
+    """Récupère le paramètre 'nom' en GET et affiche la page de salutations."""
+    nom = request.GET.get('nom', '')
+    # Empêcher des valeurs vides d'afficher une chaîne vide
+    if not nom:
+        nom = 'inconnu'
+    return render(request, 'myfirstapp/bonjour.html', {'nom': nom})
+
