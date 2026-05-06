83c51b1edf02497cb7efeca5324dc036c0936eaf
glufy100 <119037482+glufy100@users.noreply.github.com>
2026-05-06 14:01:47 +0200

    fini

--- myfirstproject/myfirstproject/settings.py
+++ myfirstproject/myfirstproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myfirstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'myfirstproject'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
